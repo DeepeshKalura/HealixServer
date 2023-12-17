f19d51b57274d4d8bfcef51be82a7a1f6c785871
DeepeshKalura <deepeshkalurs@gmail.com>
2023-12-17 22:19:57 +0530

    Add real-time audio input and text input using Gemini Pro

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Steps to solved
 User -> Real-time Audio Input --> using google cloud api
+Text Input --> Text as answer --> using gemini pro
--- main.py
+++ main.py
@@ -5,45 +5,53 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 
+# AssemblyAI
+
+import assemblyai as aai
+
 # Google docs
 
 import pathlib
 import textwrap
 import google.generativeai as genai
 
-# AssemblyAI
-
-import assemblyai as aai
 
 load_dotenv(find_dotenv())
 
 aai.settings.api_key = os.getenv("AAI_API_KEY")
 transcriber = aai.Transcriber()
-audioPath = ["HindiGretting.mp3","EnglishGretting.mp3"];
+# audioPath = ["HindiGretting.mp3","EnglishGretting.mp3"];
 # audioFile = "HindiGretting.mp3";
 # audioFile = "EnglishGretting.mp3";
+audioFile = "professionalLength.mp3";
 print("I am here");
 
 
-for audioFile in audioPath:
 
-    transcript = transcriber.transcribe(audioFile)
-    # print("Reached here")
-    print(transcript.text)
-    print("\n\n\n")
+# for audioFile in audioPath:
 
-
-
-
-
+transcript = transcriber.transcribe(audioFile)
+# print("Reached here")
+# print(transcript.text)
+# print("\n\n\n")
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+modelGeminiPro = genai.GenerativeModel('gemini-pro')
+
+messageString = transcript.text
+
+
+
+
+
+
 
 
 
 # ! This is the great start i will going to first create the audio to the 
 
-modelGeminiPro = genai.GenerativeModel('gemini-pro')
-# print("Currently here")
-# response = modelGeminiPro.generate_content("What is the purpose of life?")
+print("Currently here")  
+response = modelGeminiPro.generate_content(messageString)
+print("Reached here")
 # print("Now Reached Here")
+print(response.text)
 
