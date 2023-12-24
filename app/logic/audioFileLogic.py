b3e570e3f62e213865f92abda09635682c54b60a
DeepeshKalura <deepeshkalurs@gmail.com>
2023-12-24 18:16:52 +0530

    Refactor audioFileProceesing function and remove unused imports

--- app/logic/audioFileLogic.py
+++ app/logic/audioFileLogic.py
@@ -12,40 +12,24 @@ import assemblyai as aai
 
 # Google docs  --> Main AI bot
 
-import google.generativeai as genai
 
 # Speech To text  --> Speech-to-Text (STT)
 from gtts import gTTS
 
-
-
-
-
-import io
-
-def audioFileProceesing(audio_file):
+def audioFileProceesing(audio_file, chatWithModel):
     load_dotenv(find_dotenv())
     aai.settings.api_key = os.getenv("AAI_API_KEY")
     transcriber = aai.Transcriber()
     transcript = transcriber.transcribe(audio_file)
 
 
-    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-    model_gemini_pro = genai.GenerativeModel('gemini-pro', generation_config=genai.GenerationConfig(max_output_tokens=100))
 
     input_message_string = transcript.text
 
-    response = model_gemini_pro.generate_content(input_message_string).text
+    response = chatWithModel.Phykologist(input_message_string)  
     tts = gTTS(response, lang='en')
     # response_audio = io.BytesIO()
     # tts.save(response_audio)
     # response_audio.seek(0)
 
     tts.save("response.mp3");
-
-
-
-    
-
-
-
