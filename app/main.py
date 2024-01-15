af2672b1cdb1d2e27ad93ccffb37c16922f3a93a
DeepeshKalura <deepeshkalurs@gmail.com>
2024-01-15 16:39:22 +0530

    Refactor main.py and add new API endpoints

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import Body, FastAPI, File, UploadFile, WebSocket
-from fastapi.responses import FileResponse, StreamingResponse
 import io
 from pydantic import BaseModel
-from app.model.chatModel import ChatWithModel
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from app.model.chatModel import ChatWithModel
+import time
 
 origins = ["*"]
 
@@ -51,3 +50,65 @@ async def text_webscoket_endpoint(websocket: WebSocket):
         
         await websocket.send_text(response)
 
+
+class UserRequestMode(BaseModel):
+    name: str
+    tag: int
+    pd_code: str
+
+class session(BaseModel):
+    name: str
+
+def changeTotalTime(timestamp, user):
+    with open(user+".txt", "r") as f:
+        lines = f.readlines()
+        lines[3] = str(float(lines[3])+(float(timestamp))-float(lines[4])) + "\n"
+    with open(user+".txt", "w") as f:
+        f.writelines(lines)
+    return lines[3]
+
+@app.post("/session")
+async def session_values(username: session):
+    timestamp = time.time()
+    changeTotalTime(timestamp, username.name)
+    with open(username.name+".txt", "a") as f:
+        f.write(str(timestamp) + "\n")
+
+        return {
+            "tag": 0,
+        }
+
+@app.post("/newuser")
+async def user_values(input: UserRequestMode):
+    user_name = input.name
+    timestamp = time.time()
+    with open(user_name+".txt", "w") as f:
+        f.write(input.name + "\n")
+        f.write(str(input.tag) + "\n")
+        f.write(input.pd_code + "\n")
+        f.write("0" + "\n")
+        f.write(str(timestamp) + "\n")
+        return {
+            "name": input.name,
+            "tag": input.tag,
+            "pd_code": input.pd_code,
+            "total_session": "0",
+            "timestamp": timestamp,
+            "sessionOverTime": "0"
+        }
+
+@app.get("/theorpy")
+async def get_theorpy(username: session):
+    theorpy = "Default"
+    with open(username.name+".txt", "r") as f:
+        lines = f.readlines()
+        if(lines[2]==1):
+            theorpy = "Depression"
+        elif(lines[2]==2):
+            theorpy = "Anxiety"
+        else:
+            theorpy = "Default"
+
+    return {
+        "theorpy": theorpy
+    }
