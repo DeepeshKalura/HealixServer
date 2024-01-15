42216ca829cba36b127552f172c899dba1324eef
DeepeshKalura <deepeshkalurs@gmail.com>
2024-01-15 13:12:58 +0530

    Remove unused imports and commented code

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import Body, FastAPI, File, UploadFile, WebSocket
 from fastapi.responses import FileResponse, StreamingResponse
 import io
 from pydantic import BaseModel
-# from app.logic.chatModel import ChatWithModel
 from app.model.chatModel import ChatWithModel
 from fastapi.middleware.cors import CORSMiddleware
 import os
@@ -30,14 +29,9 @@ async def root():
     return {"message": "Backend Client for the project"}
 
 
-# @app.post("/audio")
-# async def process_audio_endpoint(audio_file: UploadFile = File(...)):
-#     return audioFileProceesing(audio_file)
-
 class AudioRequestModel(BaseModel):
     audio_url: str
 
-@app.post("/audio")
 
 @app.websocket("/text")
 async def text_webscoket_endpoint(websocket: WebSocket):
@@ -57,19 +51,3 @@ async def text_webscoket_endpoint(websocket: WebSocket):
         
         await websocket.send_text(response)
 
-@app.websocket("/blog")
-async def blog_webscoket_endpoint(websocket: WebSocket):
-
-    task = websocket.accept
-
-    chat_model = ChatWithModel()
-
-    await task()
-
-    while True:
-        data = await websocket.receive_text()
-        if(data == 'exit' or data == 'Exit'):
-            await websocket.close()
-            break
-        response = chat_model.blog(data)
-        await websocket.send_text(response)
