d854e1e02d54a5c6f2e631589d577d77b04dd275
DeepeshKalura <deepeshkalurs@gmail.com>
2024-01-24 20:26:43 +0530

    Add database connection abstraction layer

---
+++ app/database.py
@@ -0,0 +1,21 @@
+import os
+from pymongo.mongo_client import MongoClient
+
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+name=os.getenv("MONGO_NAME")
+password=os.getenv("MONGO_PASSWORD")
+
+uri = f"mongodb+srv://{name}:{password}@cluster1.ussxiyu.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+
+def get_db():
+    db = client["dev"]
+    return db
+
+#! From this code I wanted  to build an abstraction layer for the database connection
+
