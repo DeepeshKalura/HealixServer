33ed62fcd9665aab19be78310b231a9a9ed1c861
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-02 22:18:26 +0530

    Fix API key variable name in store_theme_of_user function

--- app/pre_processing.py
+++ app/pre_processing.py
@@ -25,10 +25,10 @@ def store_compound_score(sentence):
 
 
 def store_theme_of_user(sentence):
-    api = os.getenv('HUGGINGFACE_AUTH_TOKEN')
+    api_key = os.getenv('HUGGINGFACE_AUTH_TOKEN')
     candidate_labels = ['Personal', 'Love', 'Work', 'Education','Technology']
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
-    headers = {"Authorization": f"Bearer {api}"}
+    headers = {"Authorization": f"Bearer {api_key}"}
     payload = {
         "inputs": sentence,
         "parameters": {
@@ -39,14 +39,13 @@ def store_theme_of_user(sentence):
 
         response = requests.post(API_URL, headers=headers, json=payload)
         result = response.json()
-        print("\n\n\n\n")
         print(result)
-        print("\n\n\n\n")
+
 
     except Exception as e:
         print(e)
-        result = {'sequence': sentence, 'labels': ['Personal', 'Love', 'Education', 'Technology', 'Work'], 'scores': [0.5388193726539612, 0.4398689270019531, 0.01173669658601284, 0.006696830503642559, 0.0028781406581401825]}
     user_them = {}
+    result = {'sequence': sentence, 'labels': ['Personal', 'Love', 'Education', 'Technology', 'Work'], 'scores': [0.5388193726539612, 0.4398689270019531, 0.01173669658601284, 0.006696830503642559, 0.0028781406581401825]}
     if 'labels' in result and 'scores' in result:
         for i in range(5):
             
