65218b18f27a412e4abe81d97e01616c50eaa7f6
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-08 22:16:17 +0530

    Add Gunicorn configuration file and update startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 # For running the basic applications
-gunicorn -k uvicorn.workers.UvicornWorker app.main:app
+# gunicorn -k uvicorn.workers.UvicornWorker app.main:app
+gunicorn --config gunicorn.py app.main:app
 
 # Run fot the docker if you are using docker
 # docker run -d -p 3000:3100 deepeshkalura/fastapi-healix
