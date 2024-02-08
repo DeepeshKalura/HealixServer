aaf0d016f19b3f10f1dbfc65d44891ed7b18438c
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-08 11:52:20 +0530

    Remove Azure Web Apps deployment workflows

--- startup.sh
+++ startup.sh
@@ -1,2 +1,6 @@
-docker pull deepeshkalura/fastapi-healix
-docker run -d -p 3000:3100 deepeshkalura/fastapi-healix
+# For running the basic applications
+gunicorn -k uvicorn.workers.UvicornWorker -b :3100 app.main:app
+
+# Run fot the docker if you are using docker
+# docker run -d -p 3000:3100 deepeshkalura/fastapi-healix
+
