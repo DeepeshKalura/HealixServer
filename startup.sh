5d37698b082059fcefc7014696abc39011087605
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-08 16:20:16 +0530

    Remove port binding from gunicorn command

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 # For running the basic applications
-gunicorn -k uvicorn.workers.UvicornWorker -b :3100 app.main:app
+gunicorn -k uvicorn.workers.UvicornWorker app.main:app
 
 # Run fot the docker if you are using docker
 # docker run -d -p 3000:3100 deepeshkalura/fastapi-healix
