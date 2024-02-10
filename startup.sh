aea3bdda3d6d87f89037aeb2a0fad9b72f70776b
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-10 11:23:54 +0530

    Update startup.sh to use uvicorn instead of gunicorn

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 # For running the basic applications
 # gunicorn -k uvicorn.workers.UvicornWorker app.main:app
-gunicorn --config gunicorn.py app.main:app
-
+# gunicorn --config gunicorn.py app.main:app
+# uvicorn app.main:app --host
+uvicorn app.main:app --host 0.0.0.0 --port 8000
 # Run fot the docker if you are using docker
 # docker run -d -p 3000:3100 deepeshkalura/fastapi-healix
 
