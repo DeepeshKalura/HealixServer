0a47f37e41f3658989ee03e3db416c53463d25e9
DeepeshKalura <deepeshkalurs@gmail.com>
2024-02-24 20:41:15 +0530

    Add abstraction layer for database connection

--- app/database.py
+++ app/database.py
@@ -1,11 +1,27 @@
+from abc import ABC, abstractmethod
 import os
 from pymongo.mongo_client import MongoClient
-
-
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+
+class Database(ABC):
+    @abstractmethod
+    def connect_to_database(self):
+        pass
+
+    @abstractmethod
+    def store_data(self, db, collectionName):
+        pass
+
+    @abstractmethod
+    def get_data(self, db, collectionName):
+        pass
+
+
+
+# Mongo DB 
 name=os.getenv("MONGO_NAME")
 password=os.getenv("MONGO_PASSWORD")
 
@@ -20,6 +36,20 @@ def get_db():
 def get_collection(db, collectionName):
     collection = db[collectionName]
     return collection
+
+
+# Convex Database 
+
+class ConvexDatabase(Database):
+    def connect_to_database(self):
+        pass
+
+    def store_data(self, tableName: str):
+        pass
+
+    def get_data(self):
+        pass
+
 
 #! From this code I wanted  to build an abstraction layer for the database connection
 
